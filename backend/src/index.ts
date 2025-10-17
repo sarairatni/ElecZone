6876c951bfc1a8d80809dde9966acbae223eb005
sarairatni <ms_iratni@esi.dz>
2025-10-17 20:41:29 +0100

    ai removed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,14 +4,13 @@ import categoryRoutes from "./modules/category/infra/express/category.routes";
 import userRoutes from "./modules/user/infra/express/user.routes";
 import cartProductRouter from "./modules/cartproduct/infra/express/cartproduct.routes";
 import orderRoutes from "./modules/order/infra/express/order.routes";
-import aiRoutes from "./modules/ai/infra/express/ai.routes";
 import cors from "cors";
 import dotenv from "dotenv";
 import { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 const app = express();
 
-dotenv.config({ debug: true});
+dotenv.config({ debug: true });
 const coreOptions = {
   origin: ["http://localhost:5001", "http://localhost:3000"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -29,16 +28,15 @@ app.use(cors());
 
 app.use("/products", productRoutes);
 app.use("/categories", categoryRoutes);
-app.use('/cartproducts', cartProductRouter);
-app.use('/orders', orderRoutes);
+app.use("/cartproducts", cartProductRouter);
+app.use("/orders", orderRoutes);
 app.use("/users", userRoutes);
-app.use("/ai", aiRoutes);
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error('Global error handler:', err.stack || err);
-  res.status(500).json({ error: 'Internal Server Error', details: err.message });
+  console.error("Global error handler:", err.stack || err);
+  res
+    .status(500)
+    .json({ error: "Internal Server Error", details: err.message });
 });
-
-
 
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
